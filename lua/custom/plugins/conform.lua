cc48f3de1b26a2418752b880a3012de581d6e4a7
Jess Lawrence <jess@salesbricks.com>
2024-06-13 21:08:08 +0900

    lualine, vim-fugitive and cleanup

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -3,17 +3,10 @@ return {
 	{
 		"stevearc/conform.nvim",
 		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				return {
-					timeout_ms = 500,
-					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-				}
-			end,
+			notify_on_error = true,
+			format_on_save = {
+				timeout_ms = 1500,
+			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "black" },
@@ -21,6 +14,11 @@ return {
 				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
 				javascriptreact = { "prettier" },
+			},
+			formatters = {
+				black = {
+					prepend_args = { "--fast" },
+				},
 			},
 		},
 	},
